cc68abc947a8f7e76534f93ce7a50a1ef4060b43
Andre-tech671 <nyanjahiaandre@gmail.com>
2025-11-10 18:21:25 +0200

    Fix layout.js: use inter variable and add children

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,12 +11,19 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
+import { Inter } from "next/font/google";
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-inter",
+});
+
 
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={'min-h-screen flex flex-col ' +}>
+      <body className={'min-h-screen flex flex-col ' + inter.className}>
+        {children}
       </body>
     </html>
   );
