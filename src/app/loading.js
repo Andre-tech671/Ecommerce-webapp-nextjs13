980d3c81bd6eead5f309481a064055cbd9b5bd0b
Andre-tech671 <nyanjahiaandre@gmail.com>
2025-11-11 03:57:01 +0200

    Add 'use client' to error.js and loading.js

--- src/app/loading.js
+++ src/app/loading.js
@@ -1,5 +1,7 @@
-export default function Loading(){
+"use client";
+
+export default function Loading() {
     return (
-        <div> Loading....</div>
-    )
+        <div>Loading....</div>
+    );
 }
